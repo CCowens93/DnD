f96c4b283e7d7c43ce94e21b5f4b524fee026bed
Casey Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-08-20 12:52:07 -0400

    first frontend component created

--- dnd/src/App.js
+++ dnd/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Wizard from './components/wizard.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+            <p>hullo</p>
+            <BrowserRouter>
+                  <Switch>
+                        <Route exact route="/" component={Wizard}/>
+                  </Switch>
+            </BrowserRouter>
+
+
 
     </div>
   );
--- dnd/src/components/wizard.js
+++ dnd/src/components/wizard.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-class wizard extends Component {
+class wizard extends Component{
     render(){
         return(
             <div>
-
+                <p>wiz</p>
             </div>
         )
     }
