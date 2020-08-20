37b82fb8a1fd8beb87abf999c2a34e934e5f4b91
Casey Cowens <cassandracowens@Cassandras-MacBook-Pro.local>
2020-08-20 11:50:20 -0400

    initial commit

---
+++ src/main/java/com/ccowens/dungeonsanddragonsbackend/DungeonsanddragonsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.ccowens.dungeonsanddragonsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsanddragonsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsanddragonsbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccowens/dungeonsanddragonsbackend/DungeonsanddragonsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccowens.dungeonsanddragonsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsanddragonsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
